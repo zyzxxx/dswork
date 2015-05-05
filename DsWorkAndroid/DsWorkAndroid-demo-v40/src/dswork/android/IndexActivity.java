2b472c9dd0893ecec36cfac202c57aac2614ed9b
olefun <olefun@7a9fa4a9-d657-4fba-ac9f-cc92ff6ae633>
2015-05-05 15:00:11 +0000

    Rejected commit: Default (2)

--- DsWorkAndroid/DsWorkAndroid-demo-v40/src/dswork/android/IndexActivity.java
+++ DsWorkAndroid/DsWorkAndroid-demo-v40/src/dswork/android/IndexActivity.java
@@ -1,9 +1,5 @@
 package dswork.android;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
@@ -16,29 +12,35 @@ import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import dswork.android.lib.core.ui.PosPoint;
 import dswork.android.lib.core.util.WorkUtil;
 
-public class IndexActivity extends FragmentActivity implements ActionBar.TabListener 
+public class IndexActivity extends FragmentActivity implements ActionBar.TabListener
 {
 	private ActionBar actionBar;
 	private PosPoint posPoint;
 	private ViewPager mViewPager;
 	private List<Fragment> fgms;
 	private SectionsPagerAdapter mSectionsPagerAdapter;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		initIndexView();//初始化主界面
 		initPoints();//初始化指示小圆点
+		System.out.println("hi");
 	}
-	
+
 	//初始化主界面
 	private void initIndexView()
 	{
-		setContentView(R.layout.activity_index);		
-		
+		setContentView(R.layout.activity_index);
+
 		//Set up the ViewPager
 		fgms = new ArrayList<Fragment>();
 		fgms.add(new Fragment1());
@@ -52,24 +54,24 @@ public class IndexActivity extends FragmentActivity implements ActionBar.TabList
 		// Set up the action bar.
 		actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) 
+		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++)
 		{
 			actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
 		}
 	}
-	
+
 	//初始化指示小圆点
 	private void initPoints()
 	{
 		posPoint = (PosPoint) findViewById(R.id.points);
 		posPoint.initPosPoint(fgms.size());
-//		posPoint.initPosPoint(fgms.size(),"●","#ff0000","#DDDDDD",255,1);
+//    posPoint.initPosPoint(fgms.size(),"●","#ff0000","#DDDDDD",255,1);
 	}
 
 	//自定义ViewPager适配器
 	private class SectionsPagerAdapter extends FragmentPagerAdapter
 	{
-		
+
 		public SectionsPagerAdapter(FragmentManager fm) {
 			super(fm);
 		}
@@ -88,12 +90,12 @@ public class IndexActivity extends FragmentActivity implements ActionBar.TabList
 		public CharSequence getPageTitle(int position) {
 			Locale l = Locale.getDefault();
 			switch (position) {
-			case 0:
-				return getString(R.string.title_ui).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_component).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_framework).toUpperCase(l);
+				case 0:
+					return getString(R.string.title_ui).toUpperCase(l);
+				case 1:
+					return getString(R.string.title_component).toUpperCase(l);
+				case 2:
+					return getString(R.string.title_framework).toUpperCase(l);
 			}
 			return null;
 		}
@@ -102,7 +104,7 @@ public class IndexActivity extends FragmentActivity implements ActionBar.TabList
 	private class MyOnPageChangeListener extends SimpleOnPageChangeListener
 	{
 		@Override
-		public void onPageSelected(int pos) 
+		public void onPageSelected(int pos)
 		{
 			//撤换action bar tab
 			actionBar.setSelectedNavigationItem(pos);
@@ -110,29 +112,29 @@ public class IndexActivity extends FragmentActivity implements ActionBar.TabList
 			posPoint.setCurPoint(pos);
 		}
 	}
-	
+
 	//重写 ActionBar Tab 方法
 	@Override
-	public void onTabSelected(ActionBar.Tab tab,FragmentTransaction fragmentTransaction) 
+	public void onTabSelected(ActionBar.Tab tab,FragmentTransaction fragmentTransaction)
 	{
 		mViewPager.setCurrentItem(tab.getPosition());
 	}
 
 	@Override
 	public void onTabUnselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
+								FragmentTransaction fragmentTransaction) {
 	}
 
 	@Override
 	public void onTabReselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
+								FragmentTransaction fragmentTransaction) {
 	}
 
 	// ****************定义Fragment主界面布局****************
 	public static class Fragment1 extends Fragment
 	{
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			View rootView = inflater.inflate(R.layout.fragment_index_1, container, false);
 			WorkUtil.addShortcut(getActivity(), rootView, R.id.ViewPager, R.drawable.ic_launcher, "ViewPager", WorkUtil.getAppIntent("dswork.android", "dswork.android.demo.ui.ViewPagerActivity", false, null, null, null));
@@ -148,19 +150,20 @@ public class IndexActivity extends FragmentActivity implements ActionBar.TabList
 	public static class Fragment2 extends Fragment
 	{
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			View rootView = inflater.inflate(R.layout.fragment_index_2, container, false);
 			WorkUtil.addShortcut(getActivity(), rootView, R.id.alphabet, R.drawable.ic_launcher, "alphabet", WorkUtil.getAppIntent("dswork.android", "dswork.android.demo.component.AlphabetActivity",false, null, null, null));
 			WorkUtil.addShortcut(getActivity(), rootView, R.id.PullToRefresh, R.drawable.ic_launcher, "PullToRefresh", WorkUtil.getAppIntent("dswork.android", "dswork.android.demo.component.PullToRefreshActivity",false, null, null, null));
 			WorkUtil.addShortcut(getActivity(), rootView, R.id.SwipeRefreshLayout, R.drawable.ic_launcher, "SwipeRefreshLayout", WorkUtil.getAppIntent("dswork.android", "dswork.android.demo.component.SwipeRefreshActivity",false, null, null, null));
+			WorkUtil.addShortcut(getActivity(), rootView, R.id.DownloadOne, R.drawable.ic_launcher, "DownloadOne", WorkUtil.getAppIntent("dswork.android", "dswork.android.demo.component.DownloadOneActivity",false, null, null, null));
 			return rootView;
 		}
 	}
 	public static class Fragment3 extends Fragment
 	{
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			View rootView = inflater.inflate(R.layout.fragment_index_3, container, false);
 			WorkUtil.addShortcut(getActivity(), rootView, R.id.webapp, R.drawable.ic_launcher, "webApp", WorkUtil.getAppIntent("dswork.android", "dswork.android.demo.framework.app.web.view.Main_A", false, null, null, null));
