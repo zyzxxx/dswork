0b7929c052644effe6980ec0ca6b348f20e51bea
DsWork <249725997@qq.com>
2017-08-24 15:29:18 +0800

    升级servlet至最低3.0

--- DsEp/common/common/auto/AutoServlet.java
+++ DsEp/common/common/auto/AutoServlet.java
@@ -1,8 +1,10 @@
 package common.auto;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 @SuppressWarnings("serial")
+@WebServlet(name="autoload", loadOnStartup=1, urlPatterns={"/AutoServlet"})
 public class AutoServlet extends HttpServlet
 {
 	static
--- xzsp/src/common/auto/AutoServlet.java
+++ xzsp/src/common/auto/AutoServlet.java
@@ -1,6 +1,9 @@
 package common.auto;
 
+import javax.servlet.annotation.WebServlet;
+
 @SuppressWarnings("serial")
+@WebServlet(name="autoload", loadOnStartup=1, urlPatterns={"/AutoServlet"})
 public class AutoServlet extends javax.servlet.http.HttpServlet
 {
 	static
